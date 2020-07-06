cfe9a179407d0e8d15e57aa4905a06fa672766f8
ChikenZLoveR <47142453+ChikenZLoveR@users.noreply.github.com>
2020-07-06 11:23:32 +0200

    Add files via upload

--- LeCarre.py
+++ LeCarre.py
@@ -1,5 +1,6 @@
 
-int(nombre) = input("Entrez un nombre et ce magnifique programme vous en donnera le carré")
+nombre = input("Entrez un nombre et ce magnifique programme vous en donnera le carré")
+nombre = int(nombre)
 carre = nombre*nombre
 print ("le carré de votre nombre est :",carre) 
 
