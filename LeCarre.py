8ec8f14311c6ac3054fd7283ffcbd07428a9b3bb
ChikenZLoveR <47142453+ChikenZLoveR@users.noreply.github.com>
2020-07-06 11:19:46 +0200

    Add files via upload

---
+++ LeCarre.py
@@ -0,0 +1,5 @@
+
+int(nombre) = input("Entrez un nombre et ce magnifique programme vous en donnera le carré")
+carre = nombre*nombre
+print ("le carré de votre nombre est :",carre) 
+
---
+++ SimpleEchange.py
@@ -0,0 +1,9 @@
+a = 1
+b = 3
+
+print("avant inversion a =",a,"et b =",b)
+
+adef = b
+bdef = a
+
+print("apres inversion a =",adef,"et b =",bdef)
