d6efcb7934646fc4c786a31871f3bf27714978d7
ChikenZLoveR <47142453+ChikenZLoveR@users.noreply.github.com>
2020-07-06 11:51:58 +0200

    Add files via upload

---
+++ LeProduit.py
@@ -0,0 +1,12 @@
+
+nombre1 = input("Entrez le premier nombre")
+nombre1 = int(nombre1)
+nombre2 = input("Entrez le second nombre")
+nombre2 = int(nombre2)
+produit = nombre1*nombre2
+if produit < 0 :
+	print("Le produit est négatif. il est égal à ",produit)
+if produit > 0 : 
+	print("Le produit est positif. Il est égal à",produit)
+
+
